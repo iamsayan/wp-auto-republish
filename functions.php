e28cebb88ade8150b77b2a09431878c4c6b69d03
Sayan Datta <sdcreativity12@gmail.com>
2024-12-27 20:36:16 +0530

    Refactor revivepress_republish_post function to use fully qualified WP_Post class and clean up argument handling

--- functions.php
+++ functions.php
@@ -18,19 +18,17 @@ if ( ! function_exists( 'revivepress_republish_post' ) ) {
     function revivepress_republish_post( $post_id, $args = array() ) {
         $post = get_post( absint( $post_id ) );
 
-		if ( ! $post instanceof WP_Post ) {
+		if ( ! $post instanceof \WP_Post ) {
 			return;
 		}
 
         $defaults = array(
             'method'  => 'republish', // Republish method, accepts 'republish' or 'clone' (clone - premium only)
             'single'  => false,       // Republish based on meta, defaults to false
-            'instant' => true,        // On demand republish, defaults to true
         );
         $args = wp_parse_args( $args, $defaults );
 
-        $args['post_id']  = $post->ID;
-        $args['external'] = true;
+        $args['post_id'] = $post->ID;
 
         do_action( 'wpar/process_republish_post', $args ); // Don't use this action directly as it may create issues in future versions
     }
